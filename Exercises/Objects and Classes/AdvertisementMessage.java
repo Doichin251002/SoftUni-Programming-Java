8014da42dfe9fb8850a9d0ef279e3f567587e9dd
Doichin251002 <dodi_hristov@abv.bg>
2023-02-22 10:36:53 +0200

    Update AdvertisementMessage.java

--- Exercises/Objects and Classes/AdvertisementMessage.java
+++ Exercises/Objects and Classes/AdvertisementMessage.java
@@ -49,6 +49,7 @@ public class AdvertisementMessage {
             this.city = city;
         }
     }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int countMessage = Integer.parseInt(scan.nextLine());
@@ -75,28 +76,32 @@ public class AdvertisementMessage {
             System.out.printf("%s %s %s - %s%n", element.getPhrase(), element.getEvent(), element.getAuthor(), element.getCity());
         }
     }
+
     public static String selectPhrase(Random randomText) {
-        String [] phrases = new String[]{"Excellent product.", "Such a great product.", "I always use that product."
+        String[] phrases = new String[]{"Excellent product.", "Such a great product.", "I always use that product."
                 , "Best product of its category.", "Exceptional product.", "I can't live without this product."};
 
         int randomIndex = randomText.nextInt(phrases.length);
         return phrases[randomIndex];
     }
+
     public static String getEvent(Random randomText) {
-        String [] events = new String[]{"Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!"
+        String[] events = new String[]{"Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!"
                 , "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!"};
 
         int randomIndex = randomText.nextInt(events.length);
         return events[randomIndex];
     }
+
     public static String getAuthor(Random randomText) {
-        String [] authors = new String[]{"Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva"};
+        String[] authors = new String[]{"Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva"};
 
         int randomIndex = randomText.nextInt(authors.length);
         return authors[randomIndex];
     }
+
     public static String getCity(Random randomText) {
-        String [] cities = new String[]{"Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"};
+        String[] cities = new String[]{"Burgas", "Sofia", "Plovdiv", "Varna", "Ruse"};
 
         int randomIndex = randomText.nextInt(cities.length);
         return cities[randomIndex];
